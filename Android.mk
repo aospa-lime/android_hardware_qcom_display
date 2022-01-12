a9eebdc8b3417d8269fcbe71365060206bd9c50a
Gagan Malvi <malvigagan@gmail.com>
2022-01-12 11:00:20 +0100

    liblight: Do not include if TARGET_PROVIDES_LIBLIGHT is set.
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- Android.mk
+++ Android.mk
@@ -2,8 +2,11 @@ sdm-libs := sdm/libs
 display-hals := include $(sdm-libs)/utils $(sdm-libs)/core libdebug gpu_tonemapper
 
 ifneq ($(TARGET_IS_HEADLESS), true)
-    display-hals += libcopybit liblight libmemtrack hdmi_cec \
+    display-hals += libcopybit libmemtrack hdmi_cec \
                     libdrmutils libhistogram drm.vendor
+    ifneq ($(TARGET_PROVIDES_LIBLIGHT), true)
+        display-hals += liblight
+    endif
 endif
 
 display-hals += gralloc
